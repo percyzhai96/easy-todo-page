b0f6c3d9de9c77e8ef9e1a36d85b3a19d9ea2444
zhaipengju <3045908145@qq.com>
2025-05-28 18:11:53 +0800

    fix:fix-px

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   plugins: {
     'postcss-pxtorem': {
-      rootValue: 37.5, // 设计稿尺寸为 375px
-      propList: ['*'],
+      rootValue: 16, // 根字体大小
+      propList: ['*'], // 转换所有属性
+      exclude: /node_modules/ // 排除node_modules目录
     },
   },
 }
